6a1bfc35f317c3792d2a322e83e6612ea29429b8
yoligAW27 <yohanagezahagn@gmail.com>
2026-02-01 14:23:52 -0800

    urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import BookList, BookViewSet
 
 # Create a router
@@ -9,6 +10,8 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     # Existing list view
     path('books/', BookList.as_view(), name='book-list'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # new endpoint
+
 
     # Include all CRUD routes from the router
     path('', include(router.urls)),
