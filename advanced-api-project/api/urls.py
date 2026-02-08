41da2fb16b1c00796d1259255c6b2c4eb794ed39
yoligAW27 <yohanagezahagn@gmail.com>
2026-02-08 11:38:28 -0800

    Add files via upload

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AuthorViewSet, BookViewSet
+
+
+router = DefaultRouter()
+router.register(r'authors', AuthorViewSet)
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)), 
+]
