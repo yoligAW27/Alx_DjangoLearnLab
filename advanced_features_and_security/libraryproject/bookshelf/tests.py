ebdc97764ecfb855c4a40f4e186b1e6f54bbd07e
yoligAW27 <yohanagezahagn@gmail.com>
2026-02-10 05:58:59 -0800

    Delete advanced_features_and_security directory

--- advanced_features_and_security/libraryproject/bookshelf/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from .models import Author, Book
-
-class AuthorModelTest(TestCase):
-    def test_author_creation(self):
-        author = Author.objects.create(name="Test Author")
-        self.assertEqual(str(author), "Test Author")
