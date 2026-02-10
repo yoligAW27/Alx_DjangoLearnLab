0e910055f9ce1b5f3672296b4ecb4e9c3afa1059
yoligAW27 <yohanagezahagn@gmail.com>
2026-02-10 05:44:52 -0800

    admin.py

--- advanced_features_and_security/libraryproject/bookshelf/admin.py
+++ advanced_features_and_security/libraryproject/bookshelf/admin.py
@@ -1,6 +1,48 @@
 from django.contrib import admin
-from .models import Author, Book, Member
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Member)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'first_name', 'last_name', 'date_of_birth', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'date_of_birth')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {
+            'fields': (
+                'first_name',
+                'last_name',
+                'date_of_birth',
+                'profile_photo'
+            )
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'first_name',
+                'last_name',
+                'date_of_birth',
+                'profile_photo',
+                'is_staff',
+                'is_active'
+            ),
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
