6c144712db134dd0dfc9224c80d337fc6885395e
yoligAW27 <yohanagezahagn@gmail.com>
2026-02-16 05:12:47 -0800

    models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,15 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 # Create your models here.
-from django.db import models
-from django.contrib.auth.models import User
-
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    tags = TaggableManager() 
 
     def __str__(self):
         return self.title
+    
+# comment class
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
+    
+# to redirect to post detail automatically.
+from django.urls import reverse
+
+def get_absolute_url(self):
+    return reverse('post-detail', kwargs={'pk': self.pk})
+
 
