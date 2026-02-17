1f929a42b6bb2fc94075c8d4dee6b56f5232efdc
yoligAW27 <yohanagezahagn@gmail.com>
2026-02-17 03:55:40 -0800

    Add files via upload

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path
+from . import views
+from .views import (
+    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+)
+
+
+urlpatterns = [
+    path('register/', views.register_view, name = 'register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile_view, name='profile'),
+]
+
+urlpatterns += [
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),           # changed 'posts/new/' → 'post/new/'
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # changed 'edit' → 'update'
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+]
+
+from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
+
+urlpatterns += [
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+]
+
+from .views import PostByTagListView
+
+urlpatterns += [
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),
+]
